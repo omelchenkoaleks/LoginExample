860bcd2762813f00c6437eae12c2d47a801b41e7
omelchenkoaleks <omelchenkoaleks@gmail.com>
2018-12-28 12:27:32 +0200

    Реализована возможность выбрать разрешение из галереи и вставить его в ImageView. Запрашиваются необходимые разрешения.

--- app/src/main/java/com/omelchenkoaleks/loginexample/Activities/RegisterActivity.java
+++ app/src/main/java/com/omelchenkoaleks/loginexample/Activities/RegisterActivity.java
@@ -1,15 +1,81 @@
 package com.omelchenkoaleks.loginexample.Activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.omelchenkoaleks.loginexample.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView imgUserPhoto;
+    static int PRegCode = 1;
+    static int REQESTCODE = 1;
+    Uri pickedImageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        imgUserPhoto = findViewById(R.id.registerUserPhoto);
+
+        imgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 22) {
+                    checkAndRegisterForPermission();
+                } else {
+                    openGallery();
+                }
+            }
+        });
+    }
+
+    private void openGallery() {
+        // TODO: открыть намерение галереи и ждать, пока пользователь выберет изображение
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQESTCODE);
+    }
+
+    private void checkAndRegisterForPermission() {
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(RegisterActivity.this,
+                        "Please accept for required permission", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PRegCode);
+            }
+        } else {
+            openGallery();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQESTCODE && data != null) {
+
+            // пользователь имеет разрешение
+            // нужно сохранить переменную Uri
+            pickedImageUri = data.getData();
+            imgUserPhoto.setImageURI(pickedImageUri);
+        }
     }
 }
